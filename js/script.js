6d5ed0b3117bc016c787d73ad953a1e2b812b34e
Lia Stein <lastein@mac.com>
2014-03-24 23:19:42 -0400

    no parallax updated spacing

--- js/script.js
+++ js/script.js
@@ -3,21 +3,7 @@ $(document).ready(function(){
 	$(window).scroll(function () { 
 
    $('.iconz').css({
-      'top' : -($(this).scrollTop()/9)+"px"
-   }); 
-
-});
-		$(window).scroll(function () { 
-
-   $('.iconztw').css({
-      'top' : -($(this).scrollTop()/4)+"px"
-   }); 
-
-});
-				$(window).scroll(function () { 
-
-   $('.iconzthre').css({
-      'top' : -($(this).scrollTop()/4)+"px"
+      'top' : -($(this).scrollTop()/-9)+"px"
    }); 
 
 });
