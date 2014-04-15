bd7ed491dc02008c6cd648ed3860e08c45aade1a
Lia Stein <lastein@mac.com>
2014-04-14 22:46:57 -0400

    icon update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 	$(window).scroll(function () { 
 
-   $('.iconz').css({
-      'top' : -($(this).scrollTop()/-9)+"px"
+   $('.icons').css({
+      'top' : -($(this).scrollTop()/9)+"px"
    }); 
 
 });
